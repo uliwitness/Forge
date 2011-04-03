7403d8bf87458bab8d990b1a4d76fe00c176198e
Uli Kusterer <ulivc@zathras.de>
2011-04-03 12:11:41 +0200

    Track Stacksmith HEAD.

---
+++ CMakeChunkRefNode.cpp
@@ -0,0 +1,9 @@
+//
+//  CMakeChunkRefNode.cpp
+//  Forge
+//
+//  Created by Uli Kusterer on 03.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "CMakeChunkRefNode.h"
---
+++ CMakeChunkRefNode.h
@@ -0,0 +1,7 @@
+//
+//  CMakeChunkRefNode.h
+//  Forge
+//
+//  Created by Uli Kusterer on 03.04.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
